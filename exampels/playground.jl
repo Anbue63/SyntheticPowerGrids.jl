0f46ace57452df2520cad880c3504b3f34bd3c94
buettner <buettner@pik-potsdam.de>
2022-09-21 16:44:27 +0200

    new example

--- exampels/playground.jl
+++ exampels/playground.jl
@@ -4,7 +4,7 @@ using SyntheticPowerGrids
 nodal_parameters_a = Dict(:τ_Q => 8.0, :K_P => 5, :K_Q => 0.1, :V_r => 1.0, :τ_P => [0.5, 1.0 , 5.0]) # This option recover the GNN dataset grids, please do not delete it
 nodal_parameters_b = Dict(:X => 1.0, :γ => 0.2, :α => 2.0) 
 nodal_parameters_c = Dict(:X => 1.0, :γ => 0.2, :α => 2.0, :τ_Q => 8.0, :K_P => 5, :K_Q => 0.1, :V_r => 1.0, :τ_P => [0.5, 1.0 , 5.0]) 
-nodal_parameters_d = Dict(:D => 10.0, :H => 5, :Γ => 10, :V => 1.0, :Ω => 2π * 50)  # for Jakob
+nodal_parameters_d = Dict(:D => 10.0, :H => 5, :Γ => 10, :V => 1.0, :Ω => 2π * 50)  # for Jakob and Mehrnaz networks
 
 nodal_shares_a = Dict(:load_share => 0.5, :schiffer_share => 0.5)
 nodal_shares_b = Dict(:load_share => 0.5, :schmietendorf_share => 0.5)
@@ -17,8 +17,12 @@ b = PGGeneration(num_nodes = 100, nodal_parameters = nodal_parameters_b, loads =
 c = PGGeneration(num_nodes = 100, nodal_parameters = nodal_parameters_c, loads = :PQAlgebraic, lines = :PiModelLine, generation_dynamics = :Mixed, nodal_shares = nodal_shares_c)
 d = PGGeneration(num_nodes = 100, power_distribution = :Plus_Minus_1, nodal_parameters = nodal_parameters_d, loads = :PQAlgebraic, lines = :StaticLine, generation_dynamics = :SwingEqLVS, coupling = :homogenous, edge_parameters = edge_parameters, nodal_shares = nodal_shares_d, slack = false)
 
+for_mehrnaz = PGGeneration(num_nodes = 100, nodal_parameters = nodal_parameters_d, loads = :PQAlgebraic, lines = :StaticLine, generation_dynamics = :SwingEqLVS, nodal_shares = nodal_shares_d)
+
 ##
-pg, op, embedded_graph, rejections = random_PD_grid(d)
+pg, op, embedded_graph, rejections = random_PD_grid(for_mehrnaz)
+op[:, :p] # vector containing all nodal active powers
+
 #get_effective_distances(embedded_graph, mean_len_km = c.mean_len_km, shortest_line_km = c.shortest_line_km)
 
 ##
